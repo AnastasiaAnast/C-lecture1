dfcd7b3fd5bc01dc02522ab1859fc956576e1367
Anastasia_Anast <anastasia.burskaya@yandex.ru>
2023-03-05 18:20:16 +0300

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Oh no, it's Maria again!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
